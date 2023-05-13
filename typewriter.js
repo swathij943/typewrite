ca3096e94bb33f91de1721f42c5af2c5d3dcf550
swathij943 <swathij943@gmail.com>
2023-05-13 10:45:29 -0400

    Assignment on Animated Sentence.

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,12 @@
-const sentence = "hello there from lighthouse labs";
+const sentence = "hello there swathi from lighthouse labs";
+
+const allLetters = (sentence) => {
+  let time = 0;
+  for(const char of sentence) {
+    setTimeout(() => process.stdout.write(char), time);
+    time += 50;
+  };
+  setTimeout(() => process.stdout.write('\n'), time);
+};
+
+allLetters(sentence);
